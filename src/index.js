2b96b920bedbed98b1608a6e9bd03f8eed356533
Pranav Naik <naikp@Pranavs-MacBook-Air.local>
2023-04-08 00:22:20 -0500

    refresh bug fixed2

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ import {BrowserRouter} from "react-router-dom";
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
-        <BrowserRouter basename="/">
+        <BrowserRouter basename="/Fetch-Frontend-Take-Home-Exercise">
             <App />
         </BrowserRouter>
     </React.StrictMode>
